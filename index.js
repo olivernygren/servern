4407b132691162b353274ed0429534348609b7ef
Oliver Nygren <oliver.nygren@icloud.com>
2021-04-06 18:29:21 +0200

    set up first server

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-
+const http = require('http')
 const style = require('ansi-styles')
 
+const server = http.createServer((request, response) => {
+  response.write('hello world');
+  response.end();
+})
 
-
-console.log(`${style.bgCyan.open}Hello world!${style.green.close}`);
+// console.log(`${style.bgCyan.open}Hello world!${style.green.close}`);
